3af56d47440aea7415381abaa5892ac0316022c7
a <a>
2013-12-18 21:18:17 +0100

    Dbml dla bazy + struktura MVVM

--- ERES.Net/View/MainWindow.xaml.cs
+++ ERES.Net/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ERES.Net
+namespace ERES.Net.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
